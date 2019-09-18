2d2d5a1583f0deffa2cb2afc71205ee752d20d7f
yana <YanaOnushkina>
2019-09-18 17:34:18 +0200

    Created game and all elements of it

---
+++ New Unity Project/Assets/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float sideForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, sideForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -sideForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+    }
+}

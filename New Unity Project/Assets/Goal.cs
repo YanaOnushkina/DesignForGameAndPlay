873150e163ac20d8f388eb761220a792feeab166
yana <YanaOnushkina>
2019-09-19 11:48:58 +0200

    Level 1 additions

---
+++ New Unity Project/Assets/Goal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+
+    public Rigidbody player;
+    public Rigidbody platform;
+    public float mass;
+    bool hit = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Goal" && hit == false)
+        {
+
+            hit = true;
+            player.isKinematic = true;
+            platform.isKinematic = true;
+
+
+            Debug.Log("You Win!");
+
+        }
+
+    }
+
+}

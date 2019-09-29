87d5e94e5b6ccd81b634353074151b00f6c3144e
yana <YanaOnushkina>
2019-09-29 17:38:10 +0200

    Adding a stopping point

---
+++ New Unity Project/Assets/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; //used when need to change between scenes or relaod the scene that are on now
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    //public GameObject completeLevelUI;
+ 
+    public void CompleteLevel()
+    {
+
+        //completeLevelUI.SetActive(true);
+
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+
+            gameHasEnded = true;
+            Invoke("Restart", restartDelay);
+
+        }
+
+    }
+
+    void Restart()
+    {
+
+        Debug.Log("hi");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+}

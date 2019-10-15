a506f4f0dba2e26393e18aaff9805b1ddbc1515a
yana <YanaOnushkina>
2019-10-15 19:05:52 +0200

    Fixed Pause Menu, Boss level interface

--- New Unity Project/Assets/Scripts/StickToPlatform.cs
+++ New Unity Project/Assets/Scripts/StickToPlatform.cs
@@ -5,32 +5,45 @@ using UnityEngine;
 public class StickToPlatform : MonoBehaviour
 {
     public Rigidbody paws;
-    public Transform platform;
-
-    private void Start()
-    {
-
-        paws.transform.parent = platform;
-
-
-    }
-
+    public GameObject paw;
+    public Vector3 position;
+    public Transform blockRotation;
+    public Transform blockRotationOpposite;
+    public float timeBetween = 7f;
 
     private void Update()
     {
-        //Debug.Log("hey");
+        timeBetween -= Time.deltaTime;
+        if (GameObject.FindGameObjectWithTag("cat_paw") == null)
+        {
+            if(timeBetween > 0)
+            {
+                Debug.Log("paw");
+                StartCoroutine(Wait());
+                float x = -8.41f;
+                float y = 0;
+                float z = Random.Range(-8.33f, 8.33f);
+                position = new Vector3(x, y, z);
+                Instantiate(paws, position, blockRotation.rotation);
+
+            }
+            if (timeBetween <= 0)
+            {
+                Debug.Log("paw 1");
+                StartCoroutine(Wait());
+                float x = 8.41f;
+                float y = 15;
+                float z = Random.Range(-8.33f, 8.33f);
+                position = new Vector3(x, y, z);
+                Instantiate(paws, position, blockRotation.rotation);
+                timeBetween = 7f;
+            }
+        }
+
     }
 
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    Debug.Log("not stick");
-    //    Debug.Log(other.name);
-    //    Debug.Log(other.tag);
-    //    if (other.gameObject.tag == "Platform")
-    //    {
-    //        //This will make the player a child of the Obstacle
-    //        Debug.Log("stick");
-    //        paws.transform.parent = other.gameObject.transform; //Change "myPlayer" to your player
-    //    }
-    //}
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(3);
+    }
 }
---
+++ New Unity Project/Assets/TimeElapsed.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeElapsed : MonoBehaviour
+{
+    public Text time;
+    public float time_var;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        time_var -= Time.deltaTime;
+        time.text = time_var.ToString("0");
+
+    }
+}
